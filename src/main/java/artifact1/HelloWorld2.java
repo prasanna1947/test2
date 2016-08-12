bb57947d374a9c4ee7b955ae6ed8860ee1917ca0
prasanna1947 <gaddeprasanna2@gmail.com>
2016-08-12 11:46:20 -0500

    Add files via upload

---
+++ src/main/java/artifact1/HelloWorld2.java
@@ -0,0 +1,13 @@
+package artifact1;
+
+public class HelloWorld2 {
+
+	public HelloWorld2() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hi , Hello XYZ HelloWorld2");
+	}
+
+}
